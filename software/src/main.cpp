5a16d07662d85da302ee4ab2dd6f224b5e447274
Oskar <12718920+oskar2517@users.noreply.github.com>
2026-07-13 18:59:50 +0200

    add button test

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
+  pinMode(0, INPUT_PULLUP);
+
   pinMode(15, OUTPUT);
-  digitalWrite(15, HIGH);
+  digitalWrite(15, LOW);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  Serial.println(digitalRead(0));
+  delay(500);
 }
