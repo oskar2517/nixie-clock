97e941d9e4386a03c7a0c1079939865cf8daeeb3
Oskar <12718920+oskar2517@users.noreply.github.com>
2026-08-19 20:19:51 +0200

    make littlefs not format on mounting failure

--- software/src/filesystem.cpp
+++ software/src/filesystem.cpp
@@ -3,7 +3,7 @@
 bool filesystem_available = false;
 
 void filesystem_setup() {
-    filesystem_available = LittleFS.begin(true);
+    filesystem_available = LittleFS.begin(false);
 
     if (!filesystem_available) {
         Serial.println("LittleFS mount failed");
